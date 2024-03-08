5924afbba0b1746ba940be9133ead6d13a4689f0
Timon Schelling <timon.schelling@outlook.de>
2024-03-08 09:16:25 +0100

    -

--- example/hardware.nix
+++ example/hardware.nix
@@ -8,6 +8,9 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
