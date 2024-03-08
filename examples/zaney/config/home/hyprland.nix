4af6384ae31a132dbc03cc4683501bbf6f03ee11
Timon Schelling <timon.schelling@outlook.de>
2024-03-08 18:02:57 +0100

    revert 2

--- examples/zaney/config/home/hyprland.nix
+++ examples/zaney/config/home/hyprland.nix
@@ -20,157 +20,187 @@ in with lib; {
     extraConfig = let
       modifier = "SUPER";
     in concatStrings [ ''
-monitor = DP-3, 2560x1440, 1200x250, 1
-monitor = DP-2, 1920x1200, 0x0, 1, transform, 1
-monitor = DP-1, 1920x1200, 3760x0, 1, transform, 1
-monitor = ,preferred, auto, auto
+      monitor=,preferred,auto,1
+      windowrule = fullscreen, ^(wlogout)$
+      windowrule = animation fade,^(wlogout)$
+      general {
+        gaps_in = 6
+        gaps_out = 8
+        border_size = 2
+        col.active_border = rgba(${theme.base0C}ff) rgba(${theme.base0D}ff) rgba(${theme.base0B}ff) rgba(${theme.base0E}ff) 45deg
+        col.inactive_border = rgba(${theme.base00}cc) rgba(${theme.base01}cc) 45deg
+        layout = dwindle
+        resize_on_border = true
+      }
 
-# cursor settings
-env = XCURSOR_SIZE, 24
-env = WLR_NO_HARDWARE_CURSORS, 1
-
-env = PATH, /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/timon/.cargo/bin
-
-input {
-    # `compose:sclk` sets scrolllock as the compose key, needed by keyd
-    kb_options = compose:sclk
-
-    follow_mouse = 1
-    touchpad {
-        natural_scroll = no
-    }
-    sensitivity = 0 # -1.0 - 1.0, 0 means no modification
-}
-
-general {
-    gaps_in = 5
-    gaps_out = 7
-    border_size = 4
-
-    # col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-    # col.active_border = rgba(0052a5ee) rgba(760060ee) 30deg
-    # col.active_border = rgba(ffb500ee) rgba(760060ee) rgba(0052a5ee) 30deg
-    # col.active_border = rgba(ee00acee) rgba(ffb500ee) rgba(760060ee) rgba(0052a5ee) 30deg
-    # col.active_border = rgba(ffa200ee) rgba(b600a5ee) rgba(760060ee) rgba(0052a5ee) 30deg
-    # col.active_border = rgba(ff9300ee) rgba(b600a5ee) rgba(760060ee) rgba(0052a5ee) 30deg
-    # col.active_border = rgba(ff9300ee) rgba(6f004eee) rgba(6f004eee) rgba(0052a5ee) 30deg
-    # col.active_border = rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) 30deg
-    # col.active_border = rgba(ff9300ee) rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) rgba(0052a5ee) rgba(0052a5ee) rgba(008c41ee) 30deg
-    # col.active_border = rgba(ff9300ee) rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) rgba(0052a5ee) rgba(0052a5ee) rgba(04bf5bee) 30deg
-    # col.active_border = rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) rgba(0052a5ee) rgba(04bf5bee) 242deg
-    # col.active_border = rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) rgba(0052a5ee) rgba(00a64dee) 242deg
-    # col.active_border = rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) rgba(0052a5ee) rgba(00b849ee) 242deg
-    # col.active_border = rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) rgba(0091ffee) 242deg
-    # col.active_border = rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) 242deg
-    # col.active_border = rgba(ff9300ee) rgba(8f0064ee) rgba(ff9300ee) rgba(8f0064ee) 242deg
-    # col.active_border = rgba(ff9300ee) rgba(8f0064ee) 242deg
-    col.active_border = rgba(ff9300ee) rgba(8f0064ee) rgba(8f0064ee) rgba(8f0064ee) rgba(0052a5ee) 242deg
-
-    # col.active_border = rgba(007621ee)
-
-    col.inactive_border = rgba(595959aa)
-
-    layout = dwindle
-
-    cursor_inactive_timeout = 3
-
-    # resize_on_border = true
-}
-
-decoration {
-    rounding = 10
-    blur {
-        enabled = true
-        size = 3
-        passes = 1
-    }
-    drop_shadow = true
-    shadow_range = 20
-    shadow_render_power = 50
-    col.shadow = rgba(1a1a1aee)
-}
-
-animations {
-    enabled = yes
-
-    bezier = windows, 0.05, 0.9, 0.1, 1.05
-    animation = windows, 1, 7, windows
-    animation = windowsOut, 1, 7, default, popin 80%
-    animation = border, 1, 10, default
-    animation = borderangle, 1, 8, default
-    animation = fade, 1, 7, default
-    animation = workspaces, 1, 6, default
-
-    bezier = liner, 0, 0, 1, 1
-    animation = borderangle, 1, 400, liner, loop
-    # animation = borderangle, 1, 200, liner, loop
-    # animation = borderangle, 1, 1000, liner, loop
-}
-
-dwindle {
-    pseudotile = yes
-    preserve_split = yes
-}
-
-master {
-    new_is_master = true
-}
-
-gestures {
-    workspace_swipe = off
-}
-
-misc {
-    disable_hyprland_logo = true
-    disable_splash_rendering = true
-    background_color = 0x161616
-}
-
-exec-once = nu ~/.dotfiles/startup/main.nu
-
-
-$mainMod = SUPER
-
-bind = $mainMod, left, movefocus, l
-bind = $mainMod, right, movefocus, r
-bind = $mainMod, up, movefocus, u
-bind = $mainMod, down, movefocus, d
-
-bind = $mainMod, C, exec, kitty
-bind = $mainMod, Q, killactive
-bind = $mainMod, M, fullscreen
-bind = $mainMod, G, togglefloating
-bind = $mainMod, O, togglesplit
-bind = $mainMod, S, pseudo
-
-bindm = $mainMod, mouse:273, resizewindow
-
-binde = $mainMod CONTROL SHIFT, right, resizeactive, 20 0
-binde = $mainMod CONTROL SHIFT, left, resizeactive, -20 0
-binde = $mainMod CONTROL SHIFT, up, resizeactive, 0 -20
-binde = $mainMod CONTROL SHIFT, down, resizeactive, 0 20
-
-bindm = $mainMod, mouse:272, movewindow
-
-bind = $mainMod SHIFT, right, movewindow, r
-bind = $mainMod SHIFT, left, movewindow, l
-bind = $mainMod SHIFT, up, movewindow, u
-bind = $mainMod SHIFT, down, movewindow, d
-
-bind = $mainMod, mouse_down, workspace, e+1
-bind = $mainMod, mouse_up, workspace, e-1
-
-bind = ALT, space, exec, anyrun
-bind = $mainMod, 1, exec, sh -c "wezterm"
-bind = $mainMod, 2, exec, code --ozone-platform="wayland" --enable-features="WaylandWindowDecorations"
-bind = $mainMod, 3, exec, firefox
-bind = $mainMod, 4, exec, nautilus --new-window
-bind = $mainMod, 5, exec, /opt/enpass/Enpass
-bind = $mainMod, 6, exec, code-insiders --ozone-platform="wayland" --enable-features="WaylandWindowDecorations"
-bind = $mainMod, 7, exec, spotify
-bind = $mainMod, 8, exec, /opt/beeper/beeper.AppImage
-# bind = $mainMod, 9, exec,
-# bind = $mainMod, 0, exec,
+      input {
+        kb_layout = ${theKBDLayout}, ${theSecondKBDLayout}
+	kb_options = grp:alt_shift_toggle
+        kb_options=caps:super
+        follow_mouse = 1
+        touchpad {
+          natural_scroll = false
+        }
+        sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+        accel_profile = flat
+      }
+      env = NIXOS_OZONE_WL, 1
+      env = NIXPKGS_ALLOW_UNFREE, 1
+      env = XDG_CURRENT_DESKTOP, Hyprland
+      env = XDG_SESSION_TYPE, wayland
+      env = XDG_SESSION_DESKTOP, Hyprland
+      env = GDK_BACKEND, wayland
+      env = CLUTTER_BACKEND, wayland
+      env = SDL_VIDEODRIVER, ${sdl-videodriver}
+      env = QT_QPA_PLATFORM, wayland
+      env = QT_WAYLAND_DISABLE_WINDOWDECORATION, 1
+      env = QT_AUTO_SCREEN_SCALE_FACTOR, 1
+      env = MOZ_ENABLE_WAYLAND, 1
+      ${if cpuType == "vm" then ''
+        env = WLR_NO_HARDWARE_CURSORS,1
+        env = WLR_RENDERER_ALLOW_SOFTWARE,1
+      '' else ''
+      ''}
+      ${if gpuType == "nvidia" then ''
+        env = WLR_NO_HARDWARE_CURSORS,1
+      '' else ''
+      ''}
+      gestures {
+        workspace_swipe = true
+        workspace_swipe_fingers = 3
+      }
+      misc {
+        mouse_move_enables_dpms = true
+        key_press_enables_dpms = false
+      }
+      animations {
+        enabled = yes
+        bezier = wind, 0.05, 0.9, 0.1, 1.05
+        bezier = winIn, 0.1, 1.1, 0.1, 1.1
+        bezier = winOut, 0.3, -0.3, 0, 1
+        bezier = liner, 1, 1, 1, 1
+        animation = windows, 1, 6, wind, slide
+        animation = windowsIn, 1, 6, winIn, slide
+        animation = windowsOut, 1, 5, winOut, slide
+        animation = windowsMove, 1, 5, wind, slide
+        animation = border, 1, 1, liner
+        ${if borderAnim == true then ''
+          animation = borderangle, 1, 30, liner, loop
+        '' else ''
+        ''}
+        animation = fade, 1, 10, default
+        animation = workspaces, 1, 5, wind
+      }
+      decoration {
+        rounding = 10
+        drop_shadow = false
+        blur {
+            enabled = true
+            size = 5
+            passes = 3
+            new_optimizations = on
+            ignore_opacity = on
+        }
+      }
+      plugin {
+        hyprtrails {
+          color = rgba(${theme.base0A}ff)
+        }
+      }
+      exec-once = $POLKIT_BIN
+      exec-once = dbus-update-activation-environment --systemd --all
+      exec-once = systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+      exec-once = swww init
+      exec-once = waybar
+      exec-once = swaync
+      exec-once = wallsetter
+      exec-once = nm-applet --indicator
+      exec-once = swayidle -w timeout 720 'swaylock -f' timeout 800 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' before-sleep 'swaylock -f -c 000000'
+      dwindle {
+        pseudotile = true
+        preserve_split = true
+      }
+      master {
+        new_is_master = true
+      }
+      bind = ${modifier},Return,exec,${terminal}
+      bind = ${modifier}SHIFT,Return,exec,rofi-launcher
+      bind = ${modifier}SHIFT,W,exec,web-search
+      bind = ${modifier}SHIFT,N,exec,swaync-client -rs
+      ${if browser == "google-chrome" then ''
+	bind = ${modifier},W,exec,google-chrome-stable
+      '' else ''
+	bind = ${modifier},W,exec,${browser}
+      ''}
+      bind = ${modifier},E,exec,emopicker9000
+      bind = ${modifier},S,exec,screenshootin
+      bind = ${modifier},D,exec,discord
+      bind = ${modifier},O,exec,obs
+      bind = ${modifier},G,exec,gimp
+      bind = ${modifier}SHIFT,G,exec,godot4
+      bind = ${modifier},T,exec,thunar
+      bind = ${modifier},M,exec,spotify
+      bind = ${modifier},Q,killactive,
+      bind = ${modifier},P,pseudo,
+      bind = ${modifier}SHIFT,I,togglesplit,
+      bind = ${modifier},F,fullscreen,
+      bind = ${modifier}SHIFT,F,togglefloating,
+      bind = ${modifier}SHIFT,C,exit,
+      bind = ${modifier}SHIFT,left,movewindow,l
+      bind = ${modifier}SHIFT,right,movewindow,r
+      bind = ${modifier}SHIFT,up,movewindow,u
+      bind = ${modifier}SHIFT,down,movewindow,d
+      bind = ${modifier}SHIFT,h,movewindow,l
+      bind = ${modifier}SHIFT,l,movewindow,r
+      bind = ${modifier}SHIFT,k,movewindow,u
+      bind = ${modifier}SHIFT,j,movewindow,d
+      bind = ${modifier},left,movefocus,l
+      bind = ${modifier},right,movefocus,r
+      bind = ${modifier},up,movefocus,u
+      bind = ${modifier},down,movefocus,d
+      bind = ${modifier},h,movefocus,l
+      bind = ${modifier},l,movefocus,r
+      bind = ${modifier},k,movefocus,u
+      bind = ${modifier},j,movefocus,d
+      bind = ${modifier},1,workspace,1
+      bind = ${modifier},2,workspace,2
+      bind = ${modifier},3,workspace,3
+      bind = ${modifier},4,workspace,4
+      bind = ${modifier},5,workspace,5
+      bind = ${modifier},6,workspace,6
+      bind = ${modifier},7,workspace,7
+      bind = ${modifier},8,workspace,8
+      bind = ${modifier},9,workspace,9
+      bind = ${modifier},0,workspace,10
+      bind = ${modifier}SHIFT,SPACE,movetoworkspace,special
+      bind = ${modifier},SPACE,togglespecialworkspace
+      bind = ${modifier}SHIFT,1,movetoworkspace,1
+      bind = ${modifier}SHIFT,2,movetoworkspace,2
+      bind = ${modifier}SHIFT,3,movetoworkspace,3
+      bind = ${modifier}SHIFT,4,movetoworkspace,4
+      bind = ${modifier}SHIFT,5,movetoworkspace,5
+      bind = ${modifier}SHIFT,6,movetoworkspace,6
+      bind = ${modifier}SHIFT,7,movetoworkspace,7
+      bind = ${modifier}SHIFT,8,movetoworkspace,8
+      bind = ${modifier}SHIFT,9,movetoworkspace,9
+      bind = ${modifier}SHIFT,0,movetoworkspace,10
+      bind = ${modifier}CONTROL,right,workspace,e+1
+      bind = ${modifier}CONTROL,left,workspace,e-1
+      bind = ${modifier},mouse_down,workspace, e+1
+      bind = ${modifier},mouse_up,workspace, e-1
+      bindm = ${modifier},mouse:272,movewindow
+      bindm = ${modifier},mouse:273,resizewindow
+      bind = ALT,Tab,cyclenext
+      bind = ALT,Tab,bringactivetotop
+      bind = ,XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+      bind = ,XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+      binde = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      bind = ,XF86AudioPlay, exec, playerctl play-pause
+      bind = ,XF86AudioPause, exec, playerctl play-pause
+      bind = ,XF86AudioNext, exec, playerctl next
+      bind = ,XF86AudioPrev, exec, playerctl previous
+      bind = ,XF86MonBrightnessDown,exec,brightnessctl set 5%-
+      bind = ,XF86MonBrightnessUp,exec,brightnessctl set +5%
     '' ];
   };
 }
