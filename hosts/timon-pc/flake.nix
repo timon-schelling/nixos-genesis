4e8236c90dbf4e835753f8a63839a3a805c5e0c2
Timon Schelling <timon.schelling@outlook.de>
2024-03-17 23:39:05 +0100

    -

--- hosts/timon-pc/flake.nix
+++ hosts/timon-pc/flake.nix
@@ -8,40 +8,4 @@ let
       };
     };
   };
-in {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    impermanence.url = "github:nix-community/impermanence";
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
-  };
-
-  outputs = { ... }: {
-    nixosConfigurations = {
-      ${opts.host} = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          ./../../main.nix
-          {
-            config = {
-              inherit opts;
-            };
-          }
-        ];
-      };
-    };
-  };
-}
+in import ../../main.nix { inherit opts; }
--- main.nix
+++ main.nix
@@ -1,10 +1,52 @@
-{ lib, ... }:
+{ opts, ... }:
 
-let
-  utils = import ./utils.nix { inherit lib; };
-  system = utils.umport { path = ./system; };
-  user = utils.umport { path = ./user; };
-  imports = system ++ user;
-in {
-  inherit imports;
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence.url = "github:nix-community/impermanence";
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+  };
+
+  outputs = { inputs, ... }:
+
+    let
+      lib = inputs.nixpkgs.lib;
+    in
+    {
+      nixosConfigurations = {
+        ${opts.host} = lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            (
+              let
+                utils = import ./utils.nix { inherit lib; };
+                system = utils.umport { path = ./system; };
+                user = utils.umport { path = ./user; };
+                imports = system ++ user;
+              in
+              {
+                inherit imports;
+                config = {
+                  inherit opts;
+                };
+              }
+            )
+          ];
+        };
+      };
+    };
 }
