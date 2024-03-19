e02fcb47894e60236481ca0ab61e9d451019045d
Timon Schelling <timon.schelling@outlook.de>
2024-03-19 10:37:31 +0100

    -

--- hosts/timon-pc/flake.nix
+++ hosts/timon-pc/flake.nix
@@ -1,12 +1,14 @@
-let
+(let
   opts = {
     host = "timon-pc";
     drive = "/dev/nvme1n1";
     users = {
       timon = {
-
+        name = "Timon Schelling";
+        email = "me@timon.zip";
+        sudo = true;
       };
     };
   };
-  base = import ../../main.nix { inherit opts; };
-in base { inherit opts; }
+  base = import ../../main.nix;
+in base { inherit opts; })
