97e75499470857b8655206c33a95ecadd5e738c6
Timon Schelling <timon.schelling@outlook.de>
2024-03-19 12:39:06 +0100

    -

--- main.nix
+++ main.nix
@@ -2,7 +2,7 @@
 
 let
   lib = inputs.nixpkgs.lib;
-  utils = import ./utils.nix { inherit lib; };
+  utils = import ../../utils.nix { inherit lib; };
   system = utils.umport { path = ./system; };
   user = utils.umport { path = ./user; };
   imports = system ++ user;
