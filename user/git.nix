1025512c8f2c20229b0541f41bad7e26d9ccbcce
Timon Schelling <timon.schelling@outlook.de>
2024-03-19 21:10:39 +0100

    -

--- user/git.nix
+++ user/git.nix
@@ -1,13 +1,13 @@
-{ config, lib, ... }:
+# { config, lib, ... }:
 
-lib.mkMerge (lib.mapAttrsToList
-  (name: user: {
-    home-manager.users.${name} = {
-      programs.git = {
-        enable = true;
-        userName = user.name;
-        userEmail = user.email;
-      };
-    };
-  })
-  config.opts.users)
+# lib.mkMerge (lib.mapAttrsToList
+#   (name: user: {
+#     home-manager.users.${name} = {
+#       programs.git = {
+#         enable = true;
+#         userName = user.name;
+#         userEmail = user.email;
+#       };
+#     };
+#   })
+#   config.opts.users)
