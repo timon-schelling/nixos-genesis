dd7d816d15a59549cd8fa8edc5474a9f195b2542
Timon Schelling <timon.schelling@outlook.de>
2024-03-20 20:01:15 +0100

    -

--- system/persist.nix
+++ system/persist.nix
@@ -19,13 +19,16 @@
     ];
   };
 
-  systemd.services.createPersistUsersDir = {
-    description = "Create /persist/user directory with 1777 permissions for Home Manager Impermanence";
+  systemd.services."create-persist-user-dir" = {
+    description = "Create /persist/user directory for Home Manager Impermanence with 1777 permissions";
     requiredBy = [ "multi-user.target" ];
     after = [ "basic.target" ];
     before = [ "graphical-session-pre.target" ];
+    script = ''
+      ${pkgs.coreutils}/bin/mkdir -p /persist/user
+      ${pkgs.coreutils}/bin/chmod 1777 /persist/user
+    '';
     serviceConfig = {
-      ExecStart = "${pkgs.coreutils}/bin/mkdir -p /persist/user && ${pkgs.coreutils}/bin/chmod 1777 /persist/user";
       Type = "oneshot";
       RemainAfterExit = true;
       User = "root";
