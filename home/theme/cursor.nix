30cb38d8d04890de0a615eb7412b534016bb2b47
Timon Schelling <timon.schelling@outlook.de>
2024-03-22 11:11:30 +0100

    -

--- home/theme/cursor.nix
+++ home/theme/cursor.nix
@@ -3,7 +3,7 @@
 {
   home.pointerCursor = {
     gtk.enable = true;
-    package = pkgs.oreo-cursors-plus.overwrite {
+    package = pkgs.oreo-cursors-plus.override {
       cursorsConf = ''
         sizes = 8, 10, 12, 14, 16, 18, 20, 24, 32, 40, 48, 56, 64
         custom = color: #000, stroke: #fff, stroke-width: 1, stroke-opacity: 1
