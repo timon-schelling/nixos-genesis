d6da3129c3546afd6a99521d9567a0bb5dbf02d1
Timon Schelling <timon.schelling@outlook.de>
2024-03-22 11:14:07 +0100

    -

--- home/theme/cursor.nix
+++ home/theme/cursor.nix
@@ -5,11 +5,11 @@
     gtk.enable = true;
     package = pkgs.oreo-cursors-plus.override {
       cursorsConf = ''
-        sizes = 8, 10, 12, 14, 16, 18, 20, 24, 32, 40, 48, 56, 64
-        custom = color: #000, stroke: #fff, stroke-width: 1, stroke-opacity: 1
+        sizes = 12, 14, 16, 18, 20, 24, 32, 40, 48, 56, 64
+        test = color: #000, stroke: #fff, stroke-width: 1, stroke-opacity: 1
       '';
     };
-    name = "custom";
+    name = "test";
     size = 18;
   };
 }
