94e7bc49ebd330d94d3e8bc463ba3edca64a9ee8
Timon Schelling <timon.schelling@outlook.de>
2024-03-23 22:52:08 +0100

    -

--- system/login-manager.nix
+++ system/login-manager.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd \"bash -c Hyperland\" --user-menu";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd \"${pkgs.hyprland}/bin/Hyprland\" --user-menu";
       };
     };
   };
