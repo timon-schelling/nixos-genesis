7160ba3d4a47941e443a5ba80e8867ae0156397b
Timon Schelling <me@timon.zip>
2024-03-23 22:43:26 +0000

    rebuild 2024-03-23_22:43:25

--- home/waybar.nix
+++ home/waybar.nix
@@ -338,9 +338,6 @@
 in {
   programs.waybar = {
     enable = true;
-    package = pkgs.waybar.overrideAttrs (oldAttrs: {
-      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-    });
     style = css;
     settings = {mainBar = mainWaybarConfig;};
   };
