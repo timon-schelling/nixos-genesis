f7d5edc658896beb23deb465e95c03ce35dcb68a
Timon Schelling <me@timon.zip>
2024-03-24 09:38:14 +0000

    rebuild 2024-03-24_09:38:14

--- home/eww/default.nix
+++ home/eww/default.nix
@@ -1,12 +1,12 @@
 { config, lib, ... }:
 
 {
+  programs.eww = {
+    enabled = true;
+  };
+
   xdg.configFile."eww" = {
     source = ./config;
     recursive = true;
   };
-
-  programs.eww = {
-    enabled = true;
-  }
 }
