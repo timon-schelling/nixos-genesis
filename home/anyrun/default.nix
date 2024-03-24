0df2888c235172722a0ba074d3169dc3e4631207
Timon Schelling <me@timon.zip>
2024-03-24 13:41:53 +0000

    rebuild 2024-03-24_13:41:53

---
+++ home/anyrun/default.nix
@@ -0,0 +1,123 @@
+{ inputs, config, ... }:
+
+{
+  imports = [
+    inputs.anyrun.homeManagerModules.default
+  ]
+  programs.anyrun = {
+    enable = true;
+    config = {
+      plugins = [
+        inputs.anyrun.packages.${config.opts.system.platform}.applications
+        inputs.anyrun.packages.${config.opts.system.platform}.symbols
+        inputs.anyrun.packages.${config.opts.system.platform}.rink
+        inputs.anyrun.packages.${config.opts.system.platform}.websearch
+      ];
+      width = { fraction = 0.4; };
+      x = { fraction = 0.5; };
+      y = { fraction = 0.3; };
+      position = "top";
+      verticalOffset = { absolute = 0; };
+      hideIcons = false;
+      ignoreExclusiveZones = false;
+      layer = "overlay";
+      hidePluginInfo = false;
+      closeOnClick = true;
+      showResultsImmediately = true;
+      maxEntries = null;
+    };
+    extraCss = ''
+      @define-color primary #00A380;
+      @define-color primary #660053;
+      @define-color bg_0 #202020;
+      @define-color bg_1 #272727;
+      @define-color bg_2 #343434;
+      @define-color bg_3 #3F3F3F;
+      @define-color text_0 #AAAAAA;
+      @define-color text_1 #888888;
+      @define-color text_2 #666666;
+      @define-color text_3 #444444;
+
+      @define-color background @bg_0;
+      @define-color foreground @text_0;
+      @define-color launcher_border @bg_2;
+      @define-color entry_bg @bg_2;
+      @define-color entry_border @bg_2;
+      @define-color entry_selection @primary;
+      @define-color plug_icon_and_label_bg @bg_1;
+      @define-color plug_icon_and_label_fg @text_2;
+      @define-color result_bg @bg_1;
+      @define-color result_selected_bg @bg_2;
+      @define-color result_selected_fg @text_0;
+      @define-color result_selected_border @bg_2;
+
+      * {
+        all: unset;
+        font-family: "SF Pro Rounded", RecMonoLinear;
+        font-size: 16px;
+      }
+
+      box#main {
+        background: @background;
+        border: 3px solid @launcher_border;
+        border-radius: 16px;
+        padding: 14px 12px 0px 12px;
+        color: @foreground;
+      }
+
+      #entry {
+        background: @entry_bg;
+        border-radius: 12px;
+        padding: 10px;
+        border: 2px solid @entry_border;
+        font-size: 24px;
+        min-height: 44px;
+      }
+
+      #entry selection {
+        background: @entry_selection;
+      }
+
+      list#main{
+        padding: 10px 0px;
+      }
+
+      box > box#plugin.horizontal:first-child {
+        border-radius: 12px;
+        background: @plug_icon_and_label_bg;
+        color: @plug_icon_and_label_fg;
+        padding: 16px;
+      }
+
+      list#main > row {
+        margin: 2px 1px;
+      }
+
+      #match.activatable {
+        border-radius: 12px;
+        background: @result_bg;
+        margin: 0px 0px 12px 0px;
+        padding: 10px;
+        min-height: 44px;
+      }
+
+      #match:selected, #match:hover {
+        background: @result_selected_bg;
+        color: @result_selected_fg;
+        border: 2px solid @result_selected_border;
+        padding: 8px;
+      }
+
+      #match:hover {
+        opacity: 0.6;
+      }
+    '';
+
+    extraConfigFiles."websearch.ron".text = ''
+      Config(
+        prefix: "?",
+        engines: [Google, DuckDuckGo]
+      )
+    '';
+  };
+}
--- hosts/timon-pc/flake.nix
+++ hosts/timon-pc/flake.nix
@@ -10,6 +10,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
