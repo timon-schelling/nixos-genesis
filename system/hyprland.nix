e0823db1af753ff23d214a7716920efeb55f9e9a
Timon Schelling <me@timon.zip>
2024-04-02 17:50:48 +0000

    rebuild 2024-04-02_17:50:48

---
+++ system/hyprland.nix
@@ -0,0 +1,13 @@
+{ inputs, config, ... }:
+
+{
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${config.opts.system.platform}.hyprland;
+  };
+
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+}
