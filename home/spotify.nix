b921499618da9757f8bd53f158cfafdb327ba5cf
Timon Schelling <me@timon.zip>
2024-04-05 07:57:00 +0000

    rebuild 2024-04-05_07:56:59

--- home/spotify.nix
+++ home/spotify.nix
@@ -7,6 +7,14 @@
   ];
 
   home.packages = [
-    pkgs.spotify
+    pkgs.spotify..overrideAttrs (e: rec {
+      # Add arguments to the .desktop entry
+      desktopItem = e.desktopItem.override (d: {
+        exec = "${d.exec} --enable-features=UseOzonePlatform --ozone-platform=wayland";
+      });
+
+      # Update the install script to use the new .desktop entry
+      installPhase = builtins.replaceStrings [ "${e.desktopItem}" ] [ "${desktopItem}" ] e.installPhase;
+    })
   ];
 }
