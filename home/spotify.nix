cc93c351387d61d1bee4aa53a0552bf0daa47093
Timon Schelling <me@timon.zip>
2024-04-05 08:58:42 +0000

    rebuild 2024-04-05_08:58:41

--- home/spotify.nix
+++ home/spotify.nix
@@ -8,7 +8,9 @@
 
   home.packages = [
     (pkgs.runCommand "spotify-wayland" { buildInputs = [ pkgs.makeWrapper ]; } ''
-      makeWrapper ${pkgs.spotify}/bin/spotify $out/bin/spotify-wayland --set NIXOS_OZONE_WL 1
+      makeWrapper ${pkgs.spotify}/bin/spotify $out/bin/spotify --set NIXOS_OZONE_WL 1
+      mkdir -p "$out/share/applications/"
+      cp "${pkgs.spotify}/share/applications/spotify.desktop" "$out/share/applications/"
     '')
   ];
 }
