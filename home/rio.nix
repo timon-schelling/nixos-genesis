8320b6b09b21b9da06d649e3e93a259dbbbc715c
Timon Schelling <timon.schelling@outlook.de>
2024-04-08 13:13:46 +0200

    -

---
+++ home/rio.nix
@@ -0,0 +1,28 @@
+{ ... }:
+
+{
+  programs.rio = {
+    enable = true;
+    settings = {
+      cursor = "|";
+      blinking-cursor = false;
+      padding-x = 7;
+      window = {
+        width = 600;
+        height = 400;
+        mode = "Windowed";
+        foreground-opacity = 1.0;
+        background-opacity = 0.8;
+        blur = true;
+        decorations = "Disabled";
+      };
+      shell = {
+        program = "nu";
+        args = [];
+      };
+      fonts = {
+        size = 20;
+      };
+    };
+  };
+}
