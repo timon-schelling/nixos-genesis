38796aa9207f6dd15584b9661a6cb89023fd4173
Timon Schelling <me@timon.zip>
2024-04-09 19:14:00 +0000

    rebuild 2024-04-09_19:14:00

--- home/rio.nix
+++ home/rio.nix
@@ -1,33 +1,33 @@
 { ... }:
 
 {
-  programs.rio = {
-    enable = true;
-    settings = {
-      cursor = "|";
-      blinking-cursor = false;
-      padding-x = 4;
-      window = {
-        width = 600;
-        height = 400;
-        mode = "Windowed";
-        foreground-opacity = 1.0;
-        background-opacity = 1.0;
-        blur = true;
-        decorations = "Disabled";
-      };
-      shell = {
-        program = "/bin/sh";
-        args = [];
-      };
-      fonts = {
-        size = 22;
-      };
-      colors = {
-        ackground = "#1c1c1c";
-        foreground = "#aaaaaa";
-        cursor = "#aaaaaa";
-      };
-    };
-  };
+  # programs.rio = {
+  #   enable = true;
+  #   settings = {
+  #     cursor = "|";
+  #     blinking-cursor = false;
+  #     padding-x = 4;
+  #     window = {
+  #       width = 600;
+  #       height = 400;
+  #       mode = "Windowed";
+  #       foreground-opacity = 1.0;
+  #       background-opacity = 1.0;
+  #       blur = true;
+  #       decorations = "Disabled";
+  #     };
+  #     shell = {
+  #       program = "/bin/sh";
+  #       args = [];
+  #     };
+  #     fonts = {
+  #       size = 22;
+  #     };
+  #     colors = {
+  #       ackground = "#1c1c1c";
+  #       foreground = "#aaaaaa";
+  #       cursor = "#aaaaaa";
+  #     };
+  #   };
+  # };
 }
