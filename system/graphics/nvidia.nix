125ff859afcd03bb0593d735a8ac133464f8b48b
Timon Schelling <me@timon.zip>
2024-04-09 16:58:11 +0000

    rebuild 2024-04-09_16:58:11

--- system/graphics/nvidia.nix
+++ system/graphics/nvidia.nix
@@ -3,7 +3,6 @@
 {
   services.xserver.videoDrivers = ["nvidia"];
   boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
-  boot.blacklistedKernelModules = [ "nvidia-drm" ];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
