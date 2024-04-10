a6289edc53e13ba573ac229d09e73e3af972ff5f
Timon Schelling <me@timon.zip>
2024-04-10 17:58:00 +0000

    rebuild 2024-04-10_17:58:00

--- system/graphics/nvidia.nix
+++ system/graphics/nvidia.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 
 {
-  services.xserver.videoDrivers = ["nouveau"];
+  services.xserver.videoDrivers = ["nvidia"];
   boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
   hardware.nvidia = {
     modesetting.enable = true;
@@ -10,13 +10,5 @@
     open = true;
     nvidiaSettings = false;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
-
-  environment.sessionVariables = {
-    WLR_DRM_NO_ATOMIC = "1";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    LIBVA_DRIVER_NAME = "nvidia";
-    WLR_RENDERER = "gles2";
-    EGL_PLATFORM = "wayland";
   };
 }
