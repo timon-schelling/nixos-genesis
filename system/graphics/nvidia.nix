4ba700fe4962a2f2aed7646f5358969609ab4d14
Timon Schelling <me@timon.zip>
2024-04-10 14:40:41 +0000

    rebuild 2024-04-10_14:40:41

--- system/graphics/nvidia.nix
+++ system/graphics/nvidia.nix
@@ -19,7 +19,7 @@
         sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
         sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
         openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-        settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
+        settingsSha256 = "sha256-FUEwXpeUMH1DYH77/t76wF1UslkcW721x9BHasaRUaM=";
         persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
 
         patches = [ rcu_patch ];
