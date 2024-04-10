e554fc661b91c6e67162b7fb81779b2e878c3e4b
Timon Schelling <me@timon.zip>
2024-04-10 14:51:00 +0000

    rebuild 2024-04-10_14:51:00

--- system/graphics/nvidia.nix
+++ system/graphics/nvidia.nix
@@ -9,20 +9,13 @@
     powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
-    package = let
-      rcu_patch = pkgs.fetchpatch {
-        url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-        hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-      };
-    in config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
         version = "550.67";
         sha256_64bit = "sha256-mSAaCccc/w/QJh6w8Mva0oLrqB+cOSO1YMz1Se/32uI=";
         sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
         openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
         settingsSha256 = "sha256-FUEwXpeUMH1DYH77/t76wF1UslkcW721x9BHasaRUaM=";
         persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-
-        patches = [ rcu_patch ];
     };
   };
 }
