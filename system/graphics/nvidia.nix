5f02837b9b1a1815568db7b99619165066334ac9
Timon Schelling <me@timon.zip>
2024-04-10 18:09:51 +0000

    rebuild 2024-04-10_18:09:51

--- system/graphics/nvidia.nix
+++ system/graphics/nvidia.nix
@@ -7,7 +7,7 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = true;
+    open = false;
     nvidiaSettings = false;
     package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
   };
