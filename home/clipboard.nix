c5e4edcc040150347dc498b77cf5f30232fa5cd1
Timon Schelling <me@timon.zip>
2024-04-10 22:38:08 +0000

    rebuild 2024-04-10_22:38:07

--- home/clipboard.nix
+++ home/clipboard.nix
@@ -14,19 +14,23 @@
       Description = "Clipboard deamon";
       WantedBy = [ "graphical-session.target" ];
     };
-    Service.ExecStart = (libutils.mkNuScript pkgs "clipcatd-start" ''
-      env
-      mkdir ~/.config/clipcat
-      ${pkgs.clipcat}/bin/clipcatd default-config
-        | from toml
-        | update max_history 10000
-        | update desktop_notification.enable false
-        | update metrics.enable false
-        | update grpc.enable_http false
-        | to toml
-        | save -f ~/.config/clipcat/clipcatd.toml
-      ${pkgs.clipcat}/bin/clipcatd --no-daemon --replace
-    '');
+    Service = {
+      ExecStart = (libutils.mkNuScript pkgs "clipcatd-start" ''
+        env
+        mkdir ~/.config/clipcat
+        ${pkgs.clipcat}/bin/clipcatd default-config
+          | from toml
+          | update max_history 10000
+          | update desktop_notification.enable false
+          | update metrics.enable false
+          | update grpc.enable_http false
+          | to toml
+          | save -f ~/.config/clipcat/clipcatd.toml
+        ${pkgs.clipcat}/bin/clipcatd --no-daemon --replace
+      '');
+      StandardOutput = "syslog";
+      StandardError = "syslog";
+    };
     Install.WantedBy = [ "default.target" ];
   };
 }
