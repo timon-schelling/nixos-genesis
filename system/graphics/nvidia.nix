2780de8d98694c7c7da38ab1155139eb73173678
Timon Schelling <me@timon.zip>
2024-04-10 16:48:19 +0000

    rebuild 2024-04-10_16:48:19

--- system/graphics/nvidia.nix
+++ system/graphics/nvidia.nix
@@ -16,7 +16,7 @@
     WLR_DRM_NO_ATOMIC = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
     LIBVA_DRIVER_NAME = "nvidia";
-    MOZ_DISABLE_RDD_SANDBOX = "1";
+    WLR_RENDERER = "gles2";
     EGL_PLATFORM = "wayland";
   };
 }
