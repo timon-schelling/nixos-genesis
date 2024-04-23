8f4d56e2d9b200d65b9613938d9e983b45c2b05b
Timon Schelling <me@timon.zip>
2024-04-23 12:01:33 +0000

    rebuild 2024-04-23_12:01:33

--- src/modules/main/user/login/system.nix
+++ src/modules/main/user/login/system.nix
@@ -6,10 +6,10 @@
     settings = {
       GTK = {
         application_prefer_dark_theme = true;
-        cursor_theme_name = "Adwaita";
-        font_name = "SourceCodePro Regular 11";
-        icon_theme_name = "Papirus";
-        theme_name = "Colloid-Dark";
+        # cursor_theme_name = "Adwaita";
+        # font_name = "SourceCodePro Regular 11";
+        # icon_theme_name = "Papirus";
+        # theme_name = "Colloid-Dark";
       };
     };
   };
