0822aa0465f4fc7c8002cbb08baf519f5448d245
Timon Schelling <me@timon.zip>
2024-05-07 21:17:23 +0000

    rebuild 2024-05-07_21:17:23

--- src/overlays/nushell/overlay.nix
+++ src/overlays/nushell/overlay.nix
@@ -1,12 +1,3 @@
 self: super: {
-  nushell = super.nushell.overrideAttrs (_: {
-    version = "0.93.0-unstable-7a86b98f61e16ac4b1264e86d992aed7d83ca897";
-    src = self.fetchFromGitHub {
-      owner = "nushell";
-      repo = "nushell";
-      rev = "7a86b98f61e16ac4b1264e86d992aed7d83ca897";
-      sha256 = "sha256-b7rKUnnKmZAaoxQOa4aCpIU3/LUPgt8bVj00WuCSYA4=";
-    };
-    cargoHash = "";
-  });
+  nushell = import ./pkg.nix;
 }
