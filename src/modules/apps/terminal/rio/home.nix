15ae62449c732c1720b9af8bf5ec960ec65a5f0b
Timon Schelling <me@timon.zip>
2024-05-12 20:04:30 +0000

    rebuild 2024-05-12_20:04:30

--- src/modules/apps/terminal/rio/home.nix
+++ src/modules/apps/terminal/rio/home.nix
@@ -7,31 +7,15 @@
       cursor = "|";
       blinking-cursor = false;
       padding-x = 4;
-      window = {
-        width = 600;
-        height = 400;
-        mode = "Windowed";
-        foreground-opacity = 1.0;
-        background-opacity = 1.0;
-        blur = true;
-        decorations = "Disabled";
-      };
-      shell = {
-        program = "/usr/bin/env nu";
-        args = [];
-      };
-      fonts = {
-        size = 22;
-      };
       colors = {
-        ackground = "#1c1c1c";
+        background = "#1c1c1c";
         foreground = "#aaaaaa";
         cursor = "#aaaaaa";
       };
-      renderer = {
-        performance = "Low";
-        backend = "Vulkan";
+      fonts = {
+        size = 25;
       };
+      window.decorations = "Disabled";
     };
   };
 }
