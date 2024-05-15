fa89da31b6a4e93aaf8b71fd181d1ceaecddaa86
Timon Schelling <me@timon.zip>
2024-05-15 11:04:32 +0000

    rebuild 2024-05-15_11:04:32

--- src/utils/modules.nix
+++ src/utils/modules.nix
@@ -5,12 +5,22 @@ let
   dirToModulePath = dir: lib.strings.removePrefix "./" (lib.path.removePrefix modulesRoot dir);
   modulePathToEnableOptionConfigPath = path: (["modules"] + (lib.strings.splitString "/" path) + ["enable"]);
   enableOptionConfigPathToEnableOption = path:
-    lib.attrsets.setAttrByPath path (lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      example = true;
-      description = "Enables module";
-    });
+    if lib.length == 0 then
+      {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          example = true;
+          description = "Enables module";
+        };
+      }
+    else
+      {
+        ${lib.head path} = lib.mkOption {
+          type = lib.types.submodule (enableOptionConfigPathToEnableOption (lib.tail path));
+        };
+      }
+  ;
 
   mkModule = conf: dir: options: config:
     let
@@ -19,7 +29,8 @@ let
     {
       options = (enableOptionConfigPathToEnableOption enableOptionConfigPath) // options;
       config = lib.mkIf (lib.attrsets.getAttrFromPath enableOptionConfigPath conf) config;
-    };
+    }
+  ;
 in
 {
   inherit mkModule;
