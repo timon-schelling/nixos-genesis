a34f8bff95442eac1f8f8d2365affc9a4fc8da5e
Timon Schelling <me@timon.zip>
2024-05-16 08:42:06 +0000

    rebuild 2024-05-16_08:42:06

--- hosts/timon-pc/hardware.nix
+++ hosts/timon-pc/hardware.nix
@@ -8,5 +8,5 @@
 
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.amd.updateMicrocode = true;
-  home-manager.timon.modules.apps.browser.chromium.enable = true;
+  home-manager.users.timon.modules.apps.browser.chromium.enable = true;
 }
