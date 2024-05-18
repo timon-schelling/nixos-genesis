73579ee5078cd34462a5e2bbffc052fe7edf73e4
Timon Schelling <me@timon.zip>
2024-05-18 11:57:14 +0000

    rebuild 2024-05-18_11:57:14

--- src/home.nix
+++ src/home.nix
@@ -5,7 +5,7 @@
     inputs.home-manager.nixosModules.default
   ];
   options = {
-    opts.users = builtins.trace options.home-manager.users.type.functor.wrapped.functor.type options.home-manager.users;
+    opts.users = builtins.trace options.home-manager.users.type.functor.wrapped.functor.payload options.home-manager.users;
   };
   config = {
     home-manager = {
