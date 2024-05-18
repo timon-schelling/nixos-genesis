26daaa5fd740ea3f2672fbc5f6cdb311aeefe9de
Timon Schelling <me@timon.zip>
2024-05-18 10:05:17 +0000

    rebuild 2024-05-18_10:05:17

--- src/home.nix
+++ src/home.nix
@@ -5,7 +5,7 @@
     inputs.home-manager.nixosModules.default
   ];
   options = {
-    opts.users = builtins.trace (builtins.toJSON options.home-manager.users) options.home-manager.users;
+    opts.users = builtins.trace (builtins.toJSON options.home-manager.users.type) options.home-manager.users;
   };
   config = {
     home-manager = {
