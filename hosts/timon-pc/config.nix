b41162242ddfd49a902cf9fba86742745aca2920
Timon Schelling <me@timon.zip>
2024-05-18 12:28:18 +0000

    rebuild 2024-05-18_12:28:18

--- hosts/timon-pc/config.nix
+++ hosts/timon-pc/config.nix
@@ -5,17 +5,19 @@
   };
   users = {
     timon = {
-      name = "Timon Schelling";
-      email = "me@timon.zip";
-      passwordHash = "***REMOVED***";
-      sudo = true;
-      desktops.hyprhot.enable = true;
-      persist.data.folders = [
-        "dev"
-        "data"
-        "media"
-        "tmp"
-      ];
+      opts.user = {
+        name = "Timon Schelling";
+        email = "me@timon.zip";
+        passwordHash = "***REMOVED***";
+        sudo = true;
+        desktops.hyprhot.enable = true;
+        persist.data.folders = [
+          "dev"
+          "data"
+          "media"
+          "tmp"
+        ];
+      };
     };
   };
 }
