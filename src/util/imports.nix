3cb632bd48b2c5f7a389a8c08e0c9f53779391fe
Timon Schelling <me@timon.zip>
2024-06-18 20:46:29 +0000

    rebuild 2024-06-18_20:46:29

--- src/util/imports.nix
+++ src/util/imports.nix
@@ -17,7 +17,7 @@ let
         (builtins.readDir dir)
     );
   collectModule = type: dir:
-    collectModuleFile type dir ++ collectModulesSubDir type dir;
+    (collectModuleFile type dir) ++ (collectModulesSubDir type dir);
 
   systemModules = dir: collectModule "system" dir;
   homeModules = dir: collectModule "home" dir;
