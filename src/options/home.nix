0715c67edbe7a59de93bb76bab719521c29677c3
Timon Schelling <me@timon.zip>
2024-06-18 23:17:04 +0000

    rebuild 2024-06-18_23:17:04

--- src/options/home.nix
+++ src/options/home.nix
@@ -6,14 +6,14 @@
       type = lib.types.submodule {
         options = {
           system = lib.mkOption {
-            type = import ./system.nix { inherit lib; };
+            type = lib.types.anything;
             default = {};
           };
           username = lib.mkOption {
             type = lib.types.str;
           };
           user = lib.mkOption {
-            type = import ./user.nix { inherit lib; };
+            type = lib.types.anything;
             default = {};
           };
         };
