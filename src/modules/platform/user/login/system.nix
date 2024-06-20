230dfc6152cd77d0f83c8fc31d8ade64f3f6d983
Timon Schelling <me@timon.zip>
2024-06-20 00:56:16 +0000

    rebuild 2024-06-20_00:56:16

--- src/modules/platform/user/login/system.nix
+++ src/modules/platform/user/login/system.nix
@@ -43,4 +43,14 @@
       default_session = tuigreet_session;
     };
   };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
 }
