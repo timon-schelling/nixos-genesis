6716d901040496f61fc9bd865470aa51d6acf2c7
Timon Schelling <me@timon.zip>
2024-06-20 13:12:39 +0000

    rebuild 2024-06-20_13:12:39

--- src/modules/platform/user/login/system.nix
+++ src/modules/platform/user/login/system.nix
@@ -1,56 +1,107 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 {
-  # programs.regreet = {
-  #   enable = true;
-  #   settings = {
-  #     background = {
-  #       color = "#161616";
-  #     };
-  #     GTK = {
-  #       theme_name = "adw-gtk3-dark";
-  #       application_prefer_dark_theme = true;
-  #     };
-  #   };
-  #   cageArgs = [ "-s" "-m" "last" ];
-  # };
-  # services.displayManager.sddm = {
-  #   enable = true;
-  #   wayland.enable = true;
-  # };
-
-  platform.system.persist.folders = [
-    {
-      directory = "/var/cache/tuigreet";
-      user = "greeter";
-      group = "greeter";
-      mode = "0755";
-    }
-  ];
-
-  services.greetd = {
-    enable = true;
-    settings = rec {
-      tuigreet_session =
-        let
-          session = "${pkgs.hyprland}/bin/Hyprland";
-          tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
-        in
-        {
-          command = "${tuigreet} --time --remember --remember-user-session --cmd ${session}";
-          user = "greeter";
+  options = {
+    platform.system.sessions = lib.mkOption {
+      type = lib.types.listOf lib.types.submodule {
+        options = {
+          name = lib.mkOption {
+            type = lib.types.string;
+          };
+          command = lib.mkOption {
+            type = lib.types.string;
+          };
         };
-      default_session = tuigreet_session;
+      };
+      default = [];
     };
   };
 
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal";
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
+  config =
+  let
+    sessionsJSON = ;
+  in
+  {
+
+    # programs.regreet = {
+    #   enable = true;
+    #   settings = {
+    #     background = {
+    #       color = "#161616";
+    #     };
+    #     GTK = {
+    #       theme_name = "adw-gtk3-dark";
+    #       application_prefer_dark_theme = true;
+    #     };
+    #   };
+    #   cageArgs = [ "-s" "-m" "last" ];
+    # };
+    # services.displayManager.sddm = {
+    #   enable = true;
+    #   wayland.enable = true;
+    # };
+
+    platform.system.persist.folders = [
+      {
+        directory = "/var/cache/tuigreet";
+        user = "greeter";
+        group = "greeter";
+        mode = "0755";
+      }
+    ];
+
+    enviroment.systemPackages = [
+      (lib.util.nuscript.mkScript pkgs "select-session" ''
+        let sessions = "${lib.toJSON config.platform.system.sessions}" | from json
+        let sessions_number = $sessions | length
+
+        if ($sessions_number == 0) {
+          echo "No sessions available"
+          exit 1
+        }
+
+        let selection = (
+          if ($sessions_number == 1) {
+            0
+          } else {
+            $sessions | get name | input list -i -f "Select a session"
+          }
+        )
+
+        if ($selection == null) {
+          echo "No session selected"
+          exit 0
+        }
+
+        let command = $sessions | get $selection | get command
+        ^($command)
+      '')
+    ];
+
+    services.greetd = {
+      enable = true;
+      settings = rec {
+        tuigreet_session =
+          let
+            session = "select-session";
+            tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+          in
+          {
+            command = "${tuigreet} --time --remember --cmd ${session}";
+            user = "greeter";
+          };
+        default_session = tuigreet_session;
+      };
+    };
+
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal";
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+    };
   };
 }
