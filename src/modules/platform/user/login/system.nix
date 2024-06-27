1de45dc0053124b4c09192f4a063fc0aaa5d8434
Timon Schelling <me@timon.zip>
2024-06-27 16:33:00 +0000

    rebuild 2024-06-27_16:33:00

--- src/modules/platform/user/login/system.nix
+++ src/modules/platform/user/login/system.nix
@@ -19,24 +19,6 @@
 
   config = {
 
-    # programs.regreet = {
-    #   enable = true;
-    #   settings = {
-    #     background = {
-    #       color = "#161616";
-    #     };
-    #     GTK = {
-    #       theme_name = "adw-gtk3-dark";
-    #       application_prefer_dark_theme = true;
-    #     };
-    #   };
-    #   cageArgs = [ "-s" "-m" "last" ];
-    # };
-    # services.displayManager.sddm = {
-    #   enable = true;
-    #   wayland.enable = true;
-    # };
-
     platform.system.persist.folders = [
       {
         directory = "/var/cache/tuigreet";
@@ -46,47 +28,11 @@
       }
     ];
 
-    environment.systemPackages = builtins.trace config.platform.system.sessions [
-      (lib.util.nuscript.mkScript pkgs "select-session" ''
-        let sessions = '${builtins.toJSON config.platform.system.sessions}' | from json
-        let sessions_number = $sessions | length
-
-        if ($sessions_number == 0) {
-          echo "No sessions available"
-          exit 1
-        }
-
-        let selection = (
-          if ($sessions_number == 1) {
-            0
-          } else {
-            $sessions | get name | input list -i -f "Select a session"
-          }
-        )
-
-        if ($selection == null) {
-          echo "No session selected"
-          exit 0
-        }
-
-        let command = $sessions | get $selection | get command
-        ^($command)
-      '')
-    ];
-
     services.greetd = {
       enable = true;
-      settings = rec {
-        tuigreet_session =
-          let
-            session = "select-session";
-            tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
-          in
-          {
-            command = "${tuigreet} --time --remember --cmd ${session}";
-            user = "greeter";
-          };
-        default_session = tuigreet_session;
+      settings.default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -g '' --time --remember --remember-user-session --asterisks ";# --sessions ${}";
+        user = "greeter";
       };
     };
 
