22b24ba8d7aa825c82b04072a3675b67782e7220
Timon Schelling <me@timon.zip>
2024-07-02 17:09:41 +0000

    rebuild 2024-07-02_17:09:41

--- hosts/timon-pc/flake.nix
+++ hosts/timon-pc/flake.nix
@@ -1,4 +1,24 @@
 {
-  inputs = (import ../../inputs.nix);
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager?ref=master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence.url = "github:nix-community/impermanence";
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+    waybar = {
+      url = "github:Alexays/Waybar";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
   outputs = i: import ../../outputs.nix i (builtins.baseNameOf ./.);
 }
