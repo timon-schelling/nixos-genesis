ca90e074ba5d400c357af415d6898961b9e6dbf1
Timon Schelling <me@timon.zip>
2024-07-02 18:06:10 +0000

    rebuild 2024-07-02_18:06:09

--- src/user/home.nix
+++ src/user/home.nix
@@ -1,11 +1,16 @@
 { lib, ... }: {
   options = {
-    opts.user = {
-      name = lib.mkOption {
+    opts = {
+      username = lib.mkOption {
         type = lib.types.string;
       };
-      email = lib.mkOption {
-        type = lib.types.string;
+      user = {
+        name = lib.mkOption {
+          type = lib.types.string;
+        };
+        email = lib.mkOption {
+          type = lib.types.string;
+        };
       };
     };
   };
