8c824389b4cc6743f7718bdd26972049896dd8f3
Timon Schelling <me@timon.zip>
2024-07-02 18:04:49 +0000

    rebuild 2024-07-02_18:04:49

--- hosts/timon-pc/config.nix
+++ hosts/timon-pc/config.nix
@@ -7,15 +7,15 @@
     timon = {
       passwordHash = "***REMOVED***";
       admin = true;
-      persist.data.folders = [
-        "dev"
-        "data"
-        "media"
-        "tmp"
-      ];
       home = {
         name = "Timon Schelling";
         email = "me@timon.zip";
+        persist.data.folders = [
+          "dev"
+          "data"
+          "media"
+          "tmp"
+        ];
       };
       # desktops.hyprhot.enable = true;
     };
