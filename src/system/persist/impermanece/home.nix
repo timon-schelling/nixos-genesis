00b12950ad8c35fadb9f09d4f0abb073ed3abbca
Timon Schelling <me@timon.zip>
2024-07-02 23:40:10 +0000

    rebuild 2024-07-02_23:40:10

--- src/system/persist/impermanece/home.nix
+++ src/system/persist/impermanece/home.nix
@@ -8,11 +8,11 @@
   home.persistence."/persist/user/${config.opts.username}/data" = {
     directories = config.opts.user.persist.data.folders;
     files = config.opts.user.persist.data.files;
-    allowOther = false;
+    allowOther = true;
   };
   home.persistence."/persist/user/${config.opts.username}/state" = {
     directories = config.platform.user.persist.folders ++ config.opts.user.persist.state.folders;
     files = config.platform.user.persist.files ++ config.opts.user.persist.state.files;
-    allowOther = false;
+    allowOther = true;
   };
 }
