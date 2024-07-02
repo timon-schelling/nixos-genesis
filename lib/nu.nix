8b4b8b241c20473c3ef8fc51b6757bb729a51b72
Timon Schelling <me@timon.zip>
2024-07-02 18:55:23 +0000

    rebuild 2024-07-02_18:55:23

--- lib/nu.nix
+++ lib/nu.nix
@@ -1,7 +1,7 @@
 { pkgs, ... } :
 
 builtins.trace pkgs {
-  writeNuBin = name: script: pkgs.writeScriptBin "${name}" ''
+  writeNuBin = name: script: pkgs.writeScript "${name}" ''
     #!${pkgs.nushell}/bin/nu --stdin
 
     ${script}
