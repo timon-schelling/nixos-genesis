56937063eb66294bbfe1847bfa85cc8887a0c68d
Timon Schelling <me@timon.zip>
2024-07-03 00:16:40 +0000

    rebuild 2024-07-03_00:16:40

--- lib/default.nix
+++ lib/default.nix
@@ -1,12 +1,8 @@
 args:
 
 let
-  nu = import ./nu.nix args;
   imports = import ./imports.nix args;
 in
 {
-  writeNu = nu.writeNu;
-  writeNuBin = nu.writeNuBin;
-
   inherit imports;
 }
--- src/system/sudo/system.nix
+++ src/system/sudo/system.nix
@@ -1,9 +1,12 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  security.sudo.configFile = ''
-    root ALL=(ALL:ALL) ALL
+  security.sudo = {
+    configFile = ''
+      root ALL=(ALL:ALL) ALL
 
-    %admin ALL=(ALL:ALL) ALL
-  '';
+      %admin ALL=(ALL:ALL) SETENV: ALL
+    '';
+    extraRules = lib.mkForce [];
+  };
 }
