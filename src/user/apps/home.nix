4b7c1102eba5df477ba523aaeb059badeb871954
Timon Schelling <me@timon.zip>
2024-07-05 19:41:40 +0000

    rebuild 2024-07-05_19:41:40

--- src/user/apps/home.nix
+++ src/user/apps/home.nix
@@ -1,12 +1,31 @@
 { config, lib, ... }:
 
 let
+  # collectAppFile = dir:
+  #   let
+  #     path = (dir + "/app.nix");
+  #   in
+  #   if (builtins.pathExists path) then [ path ] else [];
+  # collectModulesSubDir = type: dir:
+  #   lib.lists.flatten (
+  #     lib.mapAttrsToList
+  #       (file: kind:
+  #         if kind == "directory" then
+  #           collectModule type (dir + "/${file}")
+  #         else []
+  #       )
+  #       (builtins.readDir dir)
+  #   );
+  # collectModule = type: dir:
+  #   (collectModuleFile type dir) ++ (collectModulesSubDir type dir);
   appsFolder = ./.;
   appFiles = lib.imports.type "app" appsFolder;
-  apps = lib.map (path: lib.strings.splitString "/" (lib.strings.removePrefix "./" (lib.path.removePrefix appsFolder path))) appFiles;
+  apps = lib.map (path:
+    lib.strings.splitString "/" (lib.strings.removePrefix "./" (lib.path.removePrefix appsFolder path))
+  ) appFiles;
 in
 {
-  options.apps = builtins.trace apps {
+  options.apps = builtins.trace (builtins.toJSON apps) {
 
   };
 }
