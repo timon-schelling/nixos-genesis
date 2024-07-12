45ce4bd191d78e1003926bbcf6371cc6f1d0a62c
Timon Schelling <me@timon.zip>
2024-07-12 08:49:59 +0000

    rebuild 2024-07-12_08:49:59

--- hosts/timon-pc/hardware.nix
+++ hosts/timon-pc/hardware.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-  boot.kernelParams = [ "video=DP-1:d" "video=DP-2:d" ];
+  # boot.kernelParams = [ "video=DP-1:d" "video=DP-2:d" ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
