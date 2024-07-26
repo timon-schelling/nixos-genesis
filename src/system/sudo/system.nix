64ae197d918c12af3db61983f7fea5cd052bf48c
Timon Schelling <me@timon.zip>
2024-07-26 00:13:07 +0000

    rebuild 2024-07-26_00:13:07

--- src/system/sudo/system.nix
+++ src/system/sudo/system.nix
@@ -2,9 +2,6 @@
 
 {
   security.sudo = {
-    extraConfig = ''
-      Defaults lecture = never
-    '';
     configFile = lib.mkForce ''
       root ALL=(ALL:ALL) SETENV: ALL
       %admin ALL=(ALL:ALL) SETENV: ALL
