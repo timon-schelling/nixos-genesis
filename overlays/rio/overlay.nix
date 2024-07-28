46ac0bbd0c2c6c491db8cfcc80cba92ab43dcb10
Timon Schelling <me@timon.zip>
2024-07-28 14:16:40 +0000

    rebuild 2024-07-28_14:16:40

--- overlays/rio/overlay.nix
+++ overlays/rio/overlay.nix
@@ -1,18 +1,21 @@
 inputs: self: super: {
-  rio = super.rio.overrideAttrs (drv: rec {
-    name = "rio-${version}";
-    version = "0.1.0";
-
-    src = super.fetchFromGitHub {
-      owner = "raphamorim";
-      repo = "rio";
-      rev = "refs/tags/v${version}";
-      hash = "sha256-Jp8f8u9CkY+pz6QaoWp6P6+OqsIjhzXH0eeoBiSDR0k=";
-    };
-
-    cargoDeps = drv.cargoDeps.overrideAttrs (super.lib.const {
-      inherit src;
-      outputHash = "sha256-3FirYpHxTRvXgRQACVvmwlaCNTyJ8dLbZ258qv5vbsc=";
-    });
-  });
+  rio = super.rio.overrideAttrs (drv:
+    let
+      version = "0.1.0";
+      name = "rio-${version}";
+      src = super.fetchFromGitHub {
+        owner = "raphamorim";
+        repo = "rio";
+        rev = "refs/tags/v${version}";
+        hash = "sha256-Jp8f8u9CkY+pz6QaoWp6P6+OqsIjhzXH0eeoBiSDR0k=";
+      };
+    in
+    {
+      inherit name version src;
+      cargoDeps = drv.cargoDeps.overrideAttrs (super.lib.const {
+        inherit src;
+        outputHash = "sha256-3FirYpHxTRvXgRQACVvmwlaCNTyJ8dLbZ258qv5vbsc=";
+      });
+    }
+  );
 }
