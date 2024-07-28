7877d1089986993ae20e47ce1d5694a0f6b9a6a8
Timon Schelling <me@timon.zip>
2024-07-28 15:39:17 +0000

    rebuild 2024-07-28_15:39:16

--- overlays/rio/overlay.nix
+++ overlays/rio/overlay.nix
@@ -1,3 +1,3 @@
 inputs: self: super: {
-  rio = builtins.trace inputs.rioterm-v0-0-39-nixpkgs-02fae25be09e93f3ead9a159837457ad62a29e26 inputs.rioterm-v0-0-39-nixpkgs-02fae25be09e93f3ead9a159837457ad62a29e26.rio;
+  rio = inputs.rioterm-v0-0-39-nixpkgs-02fae25be09e93f3ead9a159837457ad62a29e26.legacyPackages.rio;
 }
