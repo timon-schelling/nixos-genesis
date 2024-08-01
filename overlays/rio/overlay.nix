de23abab7870b1028330d6a78a493077f11a8ed0
Timon Schelling <me@timon.zip>
2024-08-01 22:15:44 +0000

    rebuild 2024-08-01_22:15:44

--- overlays/rio/overlay.nix
+++ overlays/rio/overlay.nix
@@ -1,3 +1,3 @@
 inputs: self: super: {
-  rio = inputs.rioterm-v0-0-39-nixpkgs-02fae25be09e93f3ead9a159837457ad62a29e26.legacyPackages.${super.system}.rio; # TODO: error
+  rio = super.callPackage ./package.nix {};
 }
