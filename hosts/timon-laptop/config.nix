30e195d4c896959f36b177dc0c483e942152772b
Timon Schelling <timon.schelling@outlook.de>
2024-08-03 18:15:21 +0200

    -

--- hosts/timon-laptop/config.nix
+++ hosts/timon-laptop/config.nix
@@ -35,5 +35,10 @@
   hardware = {
     enableRedistributableFirmware = true;
     cpu.intel.updateMicrocode = true;
+    nvidia.prime = {
+      offload.enable = true;
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0:2:0";
+    };
   };
 }
