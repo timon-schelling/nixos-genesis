4765a5bd8d2bb1b08e7a3743654cb51694e5f16a
Timon Schelling <me@timon.zip>
2024-08-04 17:27:12 +0000

    rebuild 2024-08-04_17:27:12

--- src/user/apps/browser/chromium/app.nix
+++ src/user/apps/browser/chromium/app.nix
@@ -12,7 +12,7 @@
         buildInputs = [ pkgs.makeWrapper ];
       }
       ''
-        makeWrapper ${pkgs.ungoogled-chromium}/bin/chromium $out/bin/chromium --set NIXOS_OZONE_WL 1
+        makeWrapper ${pkgs.ungoogled-chromium}/bin/chromium $out/bin/chromium --set NIXOS_OZONE_WL 1 --add-flags "--ozone-platform=wayland"
         cp -r "${pkgs.ungoogled-chromium}/share" "$out/"
       ''
     );
