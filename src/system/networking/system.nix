2a8fc5b01a53a5d1378ee01b106571cc53e3a32c
Timon Schelling <me@timon.zip>
2024-08-05 20:17:15 +0000

    -

--- src/system/networking/system.nix
+++ src/system/networking/system.nix
@@ -4,27 +4,27 @@
   # NetworkManager setup TODO: replace
   # platform.system.persist.folders = [ "/etc/NetworkManager/system-connections" ]; # persistent wifi etc.
   networking = {
-      networkmanager = {
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
+    dhcpcd.enable = false;
+    wireless = {
+      iwd = {
         enable = true;
-        wifi.backend = "iwd";
-      };
-      dhcpcd.enable = false;
-      wireless = {
-        iwd = {
-          enable = true;
-          settings = {
-            General = {
-              EnableNetworkConfiguration = true;
-            };
-            Settings = {
-              AlwaysRandomizeAddress = true;
-            };
-            Network = {
-              EnableIPv6 = false;
-            };
+        settings = {
+          General = {
+            EnableNetworkConfiguration = true;
+          };
+          Settings = {
+            AlwaysRandomizeAddress = false;
+          };
+          Network = {
+            EnableIPv6 = false;
           };
         };
       };
+    };
     hostName = config.opts.system.host;
   };
   systemd.network.wait-online.enable = false;
