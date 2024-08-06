4a27c60ce736de3d1f83a6a46bb013fd59da4f09
Timon Schelling <me@timon.zip>
2024-08-06 15:42:50 +0000

    -

--- src/system/networking/system.nix
+++ src/system/networking/system.nix
@@ -16,6 +16,7 @@
         Network = {
           EnableIPv6 = true;
           RoutePriorityOffset = 300;
+          NameResolvingService = "systemd";
         };
         Settings = {
           AutoConnect = true;
@@ -31,6 +32,16 @@
     # };
 
     hostName = config.opts.system.host;
+  };
+
+  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    dnsovertls = "true";
   };
 
   systemd.network.wait-online.enable = false;
