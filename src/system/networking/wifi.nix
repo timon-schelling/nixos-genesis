4f450c9c2282086b068502e0a926e983fcdb2626
Timon Schelling <me@timon.zip>
2024-08-08 16:38:14 +0000

    rebuild 2024-08-08_16:38:14

--- src/system/networking/wifi.nix
+++ src/system/networking/wifi.nix
@@ -1,26 +1,71 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  extension = { lib, ... }: {
+    options = {
+      opts.users = lib.mkOption {
+        type = lib.types.attrsOf (lib.types.submodule {
+          options = {
+            allowWifiSettings = lib.mkOption {
+              type = lib.types.bool;
+              default = true;
+            };
+          };
+        });
+      };
+    };
+  };
+in
 {
-  platform.system.persist.folders = [ "/var/lib/iwd" ];
-  networking = {
-    wireless.enable = false;
-    wireless.iwd = {
-      enable = true;
-      settings = {
-        General = {
-          EnableNetworkConfiguration = true;
-        };
-        Network = {
-          EnableIPv6 = true;
-          RoutePriorityOffset = 300;
-          NameResolvingService = "systemd";
-        };
-        Settings = {
-          AutoConnect = true;
-          Hidden = true;
-          AlwaysRandomizeAddress = false;
+  imports = [ extension ];
+  options = {
+
+  };
+
+  config = {
+    platform.system.persist.folders = [ "/var/lib/iwd" ];
+    networking = {
+      wireless.enable = false;
+      wireless.iwd = {
+        enable = true;
+        settings = {
+          General = {
+            EnableNetworkConfiguration = true;
+          };
+          Network = {
+            EnableIPv6 = true;
+            RoutePriorityOffset = 300;
+            NameResolvingService = "systemd";
+          };
+          Settings = {
+            AutoConnect = true;
+            Hidden = true;
+            AlwaysRandomizeAddress = false;
+          };
         };
       };
     };
+
+    users = {
+      users = lib.mkMerge (lib.mapAttrsToList
+        (name: user: {
+          ${name} = {
+            extraGroups = (if user.allowWifiSettings then [ "wifi" ] else [ ]);
+          };
+        })
+        config.opts.users
+      );
+    };
+
+    users.groups."wifi" = {};
+    services.dbus.packages = lib.mkAfter [
+      (pkgs.writeTextDir "etc/dbus-1/system.d/iwd-dbus-allow-wifi-group.conf" ''
+        <busconfig>
+          <policy group="wifi">
+            <allow send_destination="net.connman.iwd"/>
+          </policy>
+        </busconfig>
+      '')
+    ];
   };
 }
