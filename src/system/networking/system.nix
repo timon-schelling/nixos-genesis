2c323eb2b398ae2d9264c6eba39a77b8e648c97d
Timon Schelling <me@timon.zip>
2024-08-08 17:20:56 +0000

    Revert "rebuild 2024-08-08_15:56:20"
    This reverts commit 73726800d3fd156ca29b354db2e72eec9887cac9.

--- src/system/networking/system.nix
+++ src/system/networking/system.nix
@@ -1,16 +1,41 @@
-{ config, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-
-  imports = [
-    ./wifi.nix
-  ];
-
+  # NetworkManager setup TODO: replace
+  # platform.system.persist.folders = [ "/etc/NetworkManager/system-connections" ]; # persistent wifi etc.
   networking = {
+    # use iNet Wireless Daemon (instead of wpa_supplicant) for wireless device management
+    wireless.enable = false;
+    wireless.iwd = {
+      enable = true;
+
+      # All options: https://iwd.wiki.kernel.org/networkconfigurationsettings
+      settings = {
+        General = {
+          EnableNetworkConfiguration = true;
+        };
+        Network = {
+          EnableIPv6 = true;
+          RoutePriorityOffset = 300;
+          NameResolvingService = "systemd";
+        };
+        Settings = {
+          AutoConnect = true;
+          Hidden = true;
+          AlwaysRandomizeAddress = false; # for predictable local network
+        };
+      };
+    };
+
+    # networkmanager = {
+    #   enable = true;
+    #   wifi.backend = "iwd";
+    # };
+
     hostName = config.opts.system.host;
-    nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-    useNetworkd = true;
   };
+
+  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
 
   services.resolved = {
     enable = true;
