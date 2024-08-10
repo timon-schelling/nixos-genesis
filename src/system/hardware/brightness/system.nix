8ce502eeed1c8617baf9a973a9b360eab0d2687b
Timon Schelling <me@timon.zip>
2024-08-10 22:57:32 +0000

    rebuild 2024-08-10_22:57:32

---
+++ src/system/hardware/brightness/system.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  systemd.services."ddcci-load-i2c-devices" = {
+    description = "Load ddcci devices on i2c bus via sysfs";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${(pkgs.nu.writeScriptBin "ddcci-load-i2c-devices" ''
+        let ddcutil_output = ${pkgs.ddcutil}/bin/ddcutil detect -t
+        print $ddcutil_output
+
+        let device_names = $ddcutil_output | lines | where { str contains "/dev/" } | parse --regex ".*/dev/(?P<dev>i2c-.*)" | get dev
+        print $device_names
+
+        let target_files = $device_names | each { $"/sys/bus/i2c/devices/($in)/new_device" }
+        $target_files | each { |it| try { "ddcci 0x37" | save -f $it } }
+        print $target_files
+      '')}";
+      RemainAfterExit = true;
+      Restart = "on-failure";
+    };
+  };
+}
--- test.nu
+++ test.nu
@@ -1,3 +1,11 @@
-#!/nix/store/q83hxz68rxfk720avrlhsw4rd4qqyapk-nushell-0.96.1/bin/nu --stdin
+#!/usr/bin/env nu
 
-^anyrun --plugins "/nix/store/yzici8sb1fkl3dlssfjvd8fz8l8vvy8a-stdin-0.1.0/lib/libstdin.so" --hide-plugin-info true
+let ddcutil_output = nix run "nixpkgs#ddcutil" "--" detect -t
+print $ddcutil_output
+
+let device_names = $ddcutil_output | lines | where { str contains "/dev/" } | parse --regex ".*/dev/(?P<dev>i2c-.*)" | get dev
+print $device_names
+
+let target_files = $device_names | each { $"/sys/bus/i2c/devices/($in)/new_device" }
+$target_files | each { |it| try { "ddcci 0x37" | save -f $it } }
+print $target_files
