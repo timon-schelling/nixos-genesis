98708aadec8f127f15faae017789caade03a66eb
Timon Schelling <me@timon.zip>
2024-08-10 18:40:51 +0000

    -

--- src/system/kernel/system.nix
+++ src/system/kernel/system.nix
@@ -1,6 +1,23 @@
 { pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = [ "i2c-dev" "ddcci_backlight" ];
+    extraModulePackages = [
+      (pkgs.linuxKernel.packages.linux_latest_libre.ddcci-driver.overrideAttrs (old: {
+        src = pkgs.fetchFromGitLab {
+          owner = "ddcci-driver-linux";
+          repo = "ddcci-driver-linux";
+          rev = "7853cbfc28bc62e87db79f612568b25315397dd0";
+          hash = "sha256-QImfvYzMqyrRGyrS6I7ERYmteaTijd8ZRnC6+bA9OyM=";
+        };
+        patches = [];
+        postInstall = ''
+          mv $out/lib/modules/6.10.3-gnu $out/lib/modules/6.10.3
+        '';
+      }))
+    ];
+  };
   security.lockKernelModules = true;
 }
