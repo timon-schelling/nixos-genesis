0f7fdddde1bd79baa324989ebc0fa54a1364f4b8
Timon Schelling <me@timon.zip>
2024-08-10 17:51:14 +0000

    rebuild 2024-08-10_17:51:14

--- hosts/timon-pc/config.nix
+++ hosts/timon-pc/config.nix
@@ -32,16 +32,12 @@
     };
     extraModulePackages = [
       (pkgs.linuxKernel.packages.linux_latest_libre.ddcci-driver.overrideAttrs (old: {
-        patches = [
-          # (pkgs.fetchpatch {
-          #   url = "https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/commit/f53b127ca9d7fc0969c0ee3499d8c55aebfe8116.patch";
-          #   hash = "sha256-+5NiAyM0xRZnlqJSTw3oauTTC+XVwVQrr6uEoGQ6yhI=";
-          # })
-          (pkgs.fetchpatch {
-            url = "https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/commit/7853cbfc28bc62e87db79f612568b25315397dd0.patch";
-            hash = "sha256-l5wFQb+xHd3mS9LlJHr33hQck8JjcVuSUK6blVE8HU8=";
-          })
-        ];
+        src = pkgs.fetchFromGitLab {
+          owner = "ddcci-driver";
+          repo = "ddcci-driver";
+          rev = "7853cbfc28bc62e87db79f612568b25315397dd0";
+          hash = "";
+        };
       }))
     ];
     kernelModules = [ "kvm-amd" "i2c-dev" "ddcci_backlight" ];
