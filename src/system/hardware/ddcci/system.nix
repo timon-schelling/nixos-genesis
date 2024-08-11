5d9b9eaccce48e390ecff60d2721d5a310dff3d4
Timon Schelling <me@timon.zip>
2024-08-11 00:04:52 +0000

    rebuild 2024-08-11_00:04:52

---
+++ src/system/hardware/ddcci/system.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+{
+  services.udev.extraRules = ''
+    SUBSYSTEM=="i2c-dev", ACTION=="add",\
+      ATTR{name}=="NVIDIA i2c adapter*",\
+      TAG+="ddcci",\
+      TAG+="systemd",\
+      ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
+  '';
+
+  systemd.services."ddcci@.service" = {
+    enable = false;
+    description = "ddcci handler";
+    after = [ "graphical.target" ];
+    before = [ "shutdown.target" ];
+    conflicts = [ "shutdown.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${pkgs.bash}/bin/bash -c 'echo Trying to attach ddcci to %i && success=0 && i=0 && id=$(echo %i | cut -d "-" -f 2) && while ((success < 1)) && ((i++ < 5)); do /usr/bin/ddcutil getvcp 10 -b $id && { success=1 && echo ddcci 0x37 > /sys/bus/i2c/devices/%i/new_device && echo "ddcci attached to %i"; } || sleep 5; done'
+      '';
+      Restart = "no";
+    };
+  };
+}
