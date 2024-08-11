f3b350336460d747e5168942ec99f7200df3ba91
Timon Schelling <me@timon.zip>
2024-08-11 01:08:01 +0000

    rebuild 2024-08-11_01:08:01

--- src/system/hardware/ddcci/system.nix
+++ src/system/hardware/ddcci/system.nix
@@ -1,22 +1,22 @@
 { pkgs, ... }:
 
 {
-  services.udev.extraRules = ''
-    SUBSYSTEM=="i2c-dev", ACTION=="add", ATTR{name}=="NVIDIA i2c adapter*", TAG+="ddcci", TAG+="systemd", ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
-  '';
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="i2c-dev", ACTION=="add", ATTR{name}=="NVIDIA i2c adapter*", TAG+="ddcci", TAG+="systemd", ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
+  # '';
 
-  systemd.services."ddcci@" = {
-    enable = true;
-    description = "ddcci handler";
-    after = [ "graphical.target" ];
-    before = [ "shutdown.target" ];
-    conflicts = [ "shutdown.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = ''
-        ${pkgs.bash}/bin/bash -c 'echo Trying to attach ddcci to %i && success=0 && i=0 && id=$(echo %i | cut -d "-" -f 2) && while ((success < 1)) && ((i++ < 5)); do ${pkgs.ddcutil}/bin/ddcutil getvcp 10 -b $id && { success=1 && echo ddcci 0x37 > /sys/bus/i2c/devices/%i/new_device && echo "ddcci attached to %i"; } || sleep 5; done'
-      '';
-      Restart = "no";
-    };
-  };
+  # systemd.services."ddcci@" = {
+  #   enable = true;
+  #   description = "ddcci handler";
+  #   after = [ "graphical.target" ];
+  #   before = [ "shutdown.target" ];
+  #   conflicts = [ "shutdown.target" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = ''
+  #       ${pkgs.bash}/bin/bash -c 'echo Trying to attach ddcci to %i && success=0 && i=0 && id=$(echo %i | cut -d "-" -f 2) && while ((success < 1)) && ((i++ < 5)); do ${pkgs.ddcutil}/bin/ddcutil getvcp 10 -b $id && { success=1 && echo ddcci 0x37 > /sys/bus/i2c/devices/%i/new_device && echo "ddcci attached to %i"; } || sleep 5; done'
+  #     '';
+  #     Restart = "no";
+  #   };
+  # };
 }
