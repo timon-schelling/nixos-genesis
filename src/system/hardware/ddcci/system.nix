136226420b5fe662ce2ce2cc8c9e6ce0ba69fd4e
Timon Schelling <me@timon.zip>
2024-08-12 22:05:35 +0000

    rebuild 2024-08-12_22:05:35

--- src/system/hardware/ddcci/system.nix
+++ src/system/hardware/ddcci/system.nix
@@ -1,26 +1,68 @@
 { pkgs, ... }:
 
-{
-  services.udev.extraRules = ''
-    SUBSYSTEM=="i2c-dev", ACTION=="add",\
-      ATTR{name}=="NVIDIA i2c adapter*",\
-      TAG+="ddcci",\
-      TAG+="systemd",\
-      ENV{SYSTEMD_WANTS}+="nvidia-fix-ddcci@$kernel.service"
+let
+  serviceName = "zip.timon.os.System.NvidiaDdcciMonitorFix";
+  systemdServiceName = "nvidia-ddcci-monitor-fix";
+  serviceStartPkg = pkgs.nu.writeScript "ddcci-load-i2c-devices" ''
+    $env.DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/dbus/system_bus_socket"
+    ${pkgs.dbus-listen}/bin/dbus-listen --interface ${serviceName} ${scriptPkg}
   '';
+  scriptPkg = pkgs.nu.writeScript "ddcci-load-i2c-devices" ''
+    let ddcutil_output = ${pkgs.ddcutil}/bin/ddcutil detect -t
+    print $ddcutil_output
 
-  systemd.services."nvidia-fix-ddcci@" = {
-    scriptArgs = "%i";
-    script = ''
-      echo Trying to attach ddcci to $1
-      i=0
-      id=$(echo $1 | cut -d "-" -f 2)
-      if ${pkgs.ddcutil}/bin/ddcutil getvcp 10 -b $id; then
-        echo ddcci 0x37 > /sys/bus/i2c/devices/$1/new_device
-      fi
+    let device_names = $ddcutil_output | lines | where { str contains "/dev/" } | parse --regex ".*/dev/(?P<dev>i2c-.*)" | get dev
+    print $device_names
+
+    let target_files = $device_names | each { $"/sys/bus/i2c/devices/($in)/new_device" }
+    $target_files | par-each { |it| try { "ddcci 0x37" | save -f $it } }
+    print $target_files
+  '';
+  dbusService = pkgs.writeTextFile {
+    destination = "/share/dbus-1/services/${serviceName}";
+    text = ''
+      [D-BUS Service]
+      Name=${serviceName}
+      Exec=${serviceStartPkg}
+      SystemdService=openrazer-daemon.service
     '';
-    serviceConfig.Type = "oneshot";
   };
+in
+{
+  environment.systemPackages = [ pkgs.dbus-listen ];
+  services.dbus.packages = [ dbusService ];
+  systemd.user.services."${systemdServiceName}" = {
+    description = "Nvidia DDC/CI monitor fix";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "dbus";
+      BusName = "${serviceName}";
+      ExecStart = "${serviceStartPkg}";
+      Restart = "always";
+    };
+  };
+
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="i2c-dev", ACTION=="add",\
+  #     ATTR{name}=="NVIDIA i2c adapter*",\
+  #     TAG+="ddcci",\
+  #     TAG+="systemd",\
+  #     ENV{SYSTEMD_WANTS}+="nvidia-fix-ddcci@$kernel.service"
+  # '';
+
+  # systemd.services."nvidia-fix-ddcci@" = {
+  #   scriptArgs = "%i";
+  #   script = ''
+  #     echo Trying to attach ddcci to $1
+  #     i=0
+  #     id=$(echo $1 | cut -d "-" -f 2)
+  #     if ${pkgs.ddcutil}/bin/ddcutil getvcp 10 -b $id; then
+  #       echo ddcci 0x37 > /sys/bus/i2c/devices/$1/new_device
+  #     fi
+  #   '';
+  #   serviceConfig.Type = "oneshot";
+  # };
 
   # services.udev.extraRules = ''
   #   SUBSYSTEM=="i2c-dev", ACTION=="add", ATTR{name}=="NVIDIA i2c adapter*", TAG+="ddcci", TAG+="systemd", ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
