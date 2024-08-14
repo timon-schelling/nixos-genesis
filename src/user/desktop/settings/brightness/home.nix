4092afea8550c2b56c998ed8878ba6456483cc46
Timon Schelling <me@timon.zip>
2024-08-14 01:09:59 +0000

    rebuild 2024-08-14_01:09:59

--- src/user/desktop/settings/brightness/home.nix
+++ src/user/desktop/settings/brightness/home.nix
@@ -5,6 +5,7 @@
     pkgs.brightnessctl
     (pkgs.nu.writeScriptBin "monitor-set-brightness" ''
       def main [...args] {
+        try { monitor-fix-ddcci-nvidia }
         ll /sys/class/backlight | get name | path basename | par-each {
           ${pkgs.brightnessctl} -- --device $in set ...$args
         }
