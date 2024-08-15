e756484101ef95ca2121b828dae1d502304135ee
Timon Schelling <me@timon.zip>
2024-08-15 12:17:29 +0000

    rebuild 2024-08-15_12:17:29

--- overlays/chrysanthi-unicode-font/package.nix
+++ overlays/chrysanthi-unicode-font/package.nix
@@ -3,12 +3,10 @@
 stdenvNoCC.mkDerivation rec {
   name = "chrysanthi-unicode-font";
 
-  src = ./font.ttf;
-
   installPhase = ''
     runHook preInstall
 
-    mv $src ./${name}.ttf
+    mv ${./font.ttf} ./${name}.ttf
     install -Dm644 -t $out/share/fonts/truetype/ *.ttf
 
     runHook postInstall
