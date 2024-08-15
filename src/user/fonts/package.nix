9d15f403fd748726155cf175c8b6c10589959b38
Timon Schelling <me@timon.zip>
2024-08-15 16:18:17 +0000

    rebuild 2024-08-15_16:18:17

--- src/user/fonts/package.nix
+++ src/user/fonts/package.nix
@@ -8,7 +8,7 @@ stdenvNoCC.mkDerivation {
 
     mkdir -p $out/share/fonts/truetype/
     cp -r ${./ttfs}/* $out/share/fonts/truetype/
-    chmod -R 644 $out/share/fonts/truetype/
+    # chmod -R 644 $out/share/fonts/truetype/
 
     runHook postInstall
   '';
